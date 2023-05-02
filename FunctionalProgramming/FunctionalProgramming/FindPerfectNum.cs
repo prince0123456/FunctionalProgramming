e8df2098c1bb769d1abfc0c98c878f2d4aab77df
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-02 08:41:05 +0530

    Perfect Number

---
+++ FunctionalProgramming/FunctionalProgramming/FindPerfectNum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class FindPerfectNum
+    {
+        public static void PerfectNum()
+        {
+            int n, i, sum;
+            int mn, mx;
+            Console.WriteLine("Find THe perfect num within a given of rang");
+            Console.WriteLine("input the starting rang");
+            mn=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("end rang");
+            mx=Convert.ToInt32(Console.ReadLine());
+            for(n=mn; n<=mx; n++)
+            {
+                i = 1;
+                sum=0;
+                while (i < n)
+                {
+                    if(n%i==0)
+                        sum=sum+i;
+                        i++;
+                       
+                }
+                if(sum == n)
+                        Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿using FunctionalProgramming;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functionalprogramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional-Programming");
+            FindPerfectNum.PerfectNum();
+
+        }
+    }
+}
